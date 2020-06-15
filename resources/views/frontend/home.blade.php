7839d2dbcf84dc1053563f8dad9fdfcb4abe4f89
Đạt Nguyễn <53225576+tiendat21896@users.noreply.github.com>
2020-06-15 23:41:27 +0700

    111

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -92,7 +92,7 @@
                 </div>
             </div>
             <div class="row featured__filter">
-                @foreach($featureds as $f)
+                @foreach($featured as $f)
                     <div class="col-lg-3 col-md-4 col-sm-6 mix oranges fresh-meat">
                         <div class="featured__item">
                             <div class="featured__item__pic set-bg" data-setbg="{{$f->getImage()}}">
